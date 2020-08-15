fdedf9ceb0f525ddb6f992204c72b5750c6f9cbb
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-08-15 18:37:22 -0500

    00-Starter-Code

---
+++ app/src/main/java/com/maurozegarra/example/devbytesx/ui/DevByteActivity.kt
@@ -0,0 +1,12 @@
+package com.maurozegarra.example.devbytesx.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maurozegarra.example.devbytesx.R
+
+class DevByteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dev_byte_viewer)
+    }
+}
