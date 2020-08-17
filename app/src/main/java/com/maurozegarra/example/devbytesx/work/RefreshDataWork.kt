afaa3747d6834b771fef8469e81b95b0ccacf04f
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-08-17 16:47:51 -0500

    07-Create-Worker

---
+++ app/src/main/java/com/maurozegarra/example/devbytesx/work/RefreshDataWork.kt
@@ -0,0 +1,26 @@
+package com.maurozegarra.example.devbytesx.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.maurozegarra.example.devbytesx.database.getDatabase
+import com.maurozegarra.example.devbytesx.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    /**
+     * A coroutine-friendly method to do your work.
+     */
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
