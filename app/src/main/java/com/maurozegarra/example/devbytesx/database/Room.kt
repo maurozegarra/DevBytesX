4c226123901683bd2613bc633f6a296be2c10d3f
Mauro Zegarra <mauro.zegarra@gmail.com>
2020-08-17 07:59:08 -0500

    Add Database

--- app/src/main/java/com/maurozegarra/example/devbytesx/database/Room.kt
+++ app/src/main/java/com/maurozegarra/example/devbytesx/database/Room.kt
@@ -1,10 +1,8 @@
 package com.maurozegarra.example.devbytesx.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface VideoDao {
@@ -13,4 +11,24 @@ interface VideoDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
+        }
+    }
+    return INSTANCE
 }
